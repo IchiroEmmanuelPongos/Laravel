0850fbeba4ecc514314705ebc68b5ae8a2c9b03e
IchiroEmmanuelPongos <ipongos@student.apc.edu.ph>
2024-09-30 07:34:23 +0000

    ...

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,21 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\App;
+use URL;
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
      */
-    public function register(): void
+    public function boot()
     {
         //
-    }
-
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
+        if (App::environment(['staging', 'production'])) {
+            
+            URL::forceScheme('https');
+        }
+    
         //
     }
 }
